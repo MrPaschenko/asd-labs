4551064a9bd9b7d7ae6dce0d73d51a3ee2294f1a
MrPaschenko <paschenkod2@gmail.com>
2021-01-04 20:28:34 +0200

    gitignore

--- 1.1/1/cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
+++ 1.1/1/cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/C lang/1/main.c" "C:/C lang/1/cmake-build-debug/CMakeFiles/untitled.dir/main.c.obj"
+  "/home/dima/asd-labs/1.1/1/main.c" "/home/dima/asd-labs/1.1/1/cmake-build-debug/CMakeFiles/untitled.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
